9da8cb815a7dd6e62a09c2d3304a1aae663788e3
Francis Seneve <francisseneve@gmail.com>
2025-03-29 00:39:19 +0100

    Completed file upload

--- src/main/java/com/digisene/movie_api/controller/FileController.java
+++ src/main/java/com/digisene/movie_api/controller/FileController.java
@@ -1,4 +1,41 @@
 package com.digisene.movie_api.controller;
 
+import com.digisene.movie_api.service.FileService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+@RequestMapping("/file/")
 public class FileController {
+    private final FileService fileService;
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @Value("${project.poster}")
+    private String posterPath;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestPart(value = "file") MultipartFile file) throws IOException {
+       String uploadedFileName = fileService.upload(posterPath, file);
+       return ResponseEntity.ok().body("File uploaded" + uploadedFileName);
+    }
+
+    @GetMapping("/{fileName}")
+    public void serveFileHandler(@PathVariable String fileName, HttpServletResponse response) throws IOException {
+        InputStream resourceFile = fileService.getResourceFile(posterPath, fileName);
+        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+        StreamUtils.copy(resourceFile, response.getOutputStream());
+    }
 }
